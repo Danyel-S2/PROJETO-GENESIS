8fcec200ae967df6cd752d6bdef70e8583207c32
brunamenezes1 <brunamenezes170920@gmail.com>
2025-08-24 12:39:26 -0300

    Mudanças

--- jogo.js
+++ jogo.js
@@ -9,10 +9,10 @@ function atualizar(estado) {
   if (novaX < 0) {
     novaX = 0
     novoVx *= -1
-  }
-  if (!estado.bola.lancada) {
+   if (!estado.bola.lancada) {
     return {
-      ...estado,  cesta: { ...estado.cesta, x: novaX, vx: novoVx }
+      ...estado,
+      cesta: { ...estado.cesta, x: novaX, vx: novoVx }
     }
   }
   const novaY = estado.bola.y + estado.bola.vy
@@ -22,6 +22,7 @@ function atualizar(estado) {
     estado.bola.x < estado.cesta.x + estado.cesta.w &&
     estado.bola.y < estado.cesta.y + estado.cesta.h &&
     estado.bola.y > estado.cesta.y - estado.bola.r
+    
   if (dentroCesta) {
     return {...estado, cesta: { ...estado.cesta, x: novaX, vx: novoVx },
       bola: { x: canvas.width / 2, y: 460, r: 15, vy: 0, lancada: false }, pontos: estado.pontos + 1
