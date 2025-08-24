794de79b94f5a2114f6ad16f7c944eaee62e3f4b
brunamenezes1 <brunamenezes170920@gmail.com>
2025-08-24 13:13:50 -0300

    mudanças?

--- jogo.js
+++ jogo.js
@@ -50,28 +50,50 @@ function atualizar(estado) {
     novoVx *= -1
   }
 
-  if (dentroCesta) {
+ if (novaX < 0) {
+    novaX = 0
+    novoVx *= -1
+  }
+
+  if (!estado.bola.lancada) {
     return {
       ...estado,
-      bola: { ...estado.bola, y: 460, velocidade: 0, lancada: false },
+      cesta: { ...estado.cesta, x: novaX, vx: novoVx }
+    }
+  }
+
+  const novaY = estado.bola.y + estado.bola.vy
+  const novaVy = estado.bola.vy + 0.5
+
+  const dentroCesta =
+    estado.bola.x > estado.cesta.x &&
+    estado.bola.x < estado.cesta.x + estado.cesta.w &&
+    estado.bola.y < estado.cesta.y + estado.cesta.h &&
+    estado.bola.y > estado.cesta.y - estado.bola.r
+if (dentroCesta) {
+    return {
+      ...estado,
+      cesta: { ...estado.cesta, x: novaX, vx: novoVx },
+      bola: { x: canvas.width / 2, y: 460, r: 15, vy: 0, lancada: false },
       pontos: estado.pontos + 1
-    };
+    }
   }
 
   // A bola voltou pro chão
   if (novaY > 460) {
     return {
       ...estado,
-      bola: { ...estado.bola, y: 460, velocidade: 0, lancada: false }
-    };
+      cesta: { ...estado.cesta, x: novaX, vx: novoVx },
+      bola: { x: canvas.width / 2, y: 460, r: 15, vy: 0, lancada: false }
+    }
   }
 
   return {
     ...estado,
-    bola: { ...estado.bola, y: novaY, velocidade: novaVy, lancada: true }
-  };
-};
-
+    cesta: { ...estado.cesta, x: novaX, vx: novoVx },
+    bola: { ...estado.bola, y: novaY, vy: novaVy, lancada: true }
+  }
+}
 // Função pura: Deve lançar a bola
 const lancar = (estado) => {
   if (!estado.bola.lancada) {
