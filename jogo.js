8cbaa66cd0935179d1aa29bbe3086b6e54826ec3
brunamenezes1 <brunamenezes170920@gmail.com>
2025-08-26 13:46:13 -0300

    continuando mudanças.tst

--- jogo.js
+++ jogo.js
@@ -53,32 +53,33 @@ const atualizar = (estado) => {
 
   const novaY = estado.bola.y + estado.bola.vy
   const novaVy = estado.bola.vy + 0.5
-  
+
   const dentroCesta =
-    estado.bola.x > estado.cesta.x &&
-    estado.bola.x < estado.cesta.x + estado.cesta.w &&
-    estado.bola.y < estado.cesta.y + estado.cesta.h &&
-    estado.bola.y > estado.cesta.y - estado.bola.r
+    estado.bola.x > cestaCorrigida.x &&
+    estado.bola.x < cestaCorrigida.x + cestaCorrigida.w &&
+    estado.bola.y < cestaCorrigida.y + cestaCorrigida.h &&
+    estado.bola.y > cestaCorrigida.y - estado.bola.r
 
   if (dentroCesta) {
     return {
       ...estado,
-      cesta: { ...estado.cesta, x: novaX, vx: novoVx },
+      cesta: cestaCorrigida,
       bola: { x: canvas.width / 2, y: 460, r: 15, vy: 0, lancada: false },
       pontos: estado.pontos + 1
     }
   }
-  // A bola voltou pro chão
+
   if (novaY > 460) {
     return {
       ...estado,
-      cesta: { ...estado.cesta, x: novaX, vx: novoVx },
+      cesta: cestaCorrigida,
       bola: { x: canvas.width / 2, y: 460, r: 15, vy: 0, lancada: false }
     }
   }
+
   return {
     ...estado,
-    cesta: { ...estado.cesta, x: novaX, vx: novoVx },
+    cesta: cestaCorrigida,
     bola: { ...estado.bola, y: novaY, vy: novaVy, lancada: true }
   }
 }
